78323b4eef265b0e2e05bd62031e91b3a53d2d84
Oskar <oskar.straszynski@gmail.com>
2022-12-11 11:03:32 +0100

    faulty calculator

---
+++ src/Calculator/Calculator.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+function Calculator() {
+  const [displayValue, setDisplayValue] = useState("0");
+  const [history, setHistory] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [operand, setOperand] = useState(null);
+
+  // calculator logic goes here...
+    function handleNumberClick(number) {
+      setDisplayValue(displayValue === '0' ? number : displayValue + number);
+    }
+
+
+    function handleOperatorClick(newOperator) {
+    const currentValue = parseFloat(displayValue);
+    if (operand === null) {
+      setOperand(currentValue);
+    } else if (operator) {
+      const result = operate(operator, operand, currentValue);
+      setOperand(result);
+      setDisplayValue(String(result));
+    }
+    setOperator(newOperator);
+  }
+  function operate(operator, a, b) {
+    switch (operator) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "*":
+        return a * b;
+      case "/":
+        return a / b;
+      default:
+        return null;
+    }
+  }
+
+  const numberList = [];
+  for (let i = 0; i < 10; i++) {
+    numberList.push(
+      <button key={i+'button'} onClick={() => handleNumberClick(`${i}`)}>{i}</button>
+    );
+  }
+  function handleClearClick() {
+    setDisplayValue("0");
+  }
+  return (
+    <div>
+      <div>{displayValue}</div>
+      {numberList}
+      <button onClick={() => handleClearClick()}>AC</button>
+      <button onClick={() => handleOperatorClick("+")}>+</button>
+      <button onClick={() => handleOperatorClick("-")}>*</button>
+      <button onClick={() => handleOperatorClick("/")}>/</button>
+      <button onClick={() => handleOperatorClick("*")}>*</button>
+      <button onClick={() => handleOperatorClick("=")}>=</button>
+    </div>
+  );
+}
+export default Calculator;
