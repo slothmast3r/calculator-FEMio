97b722fc57e54f4b3d5cfc36ffbea09639c3257d
Oskar <oskar.straszynski@gmail.com>
2022-12-12 16:50:48 +0100

    some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Calculator from "./Calculator/Calculator";
 
 function App() {
--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,18 +1,107 @@
 import { useState } from "react";
+import "./Calculator.scss"
+const calculatorButtons = [
+  {
+    name: "7",
+    type: "number",
+    id: "number7",
+  },
+  {
+    name: "8",
+    type: "number",
+    id: "number8",
+  },
+  {
+    name: "9",
+    type: "number",
+    id: "number9",
+  },
+  {
+    name: "DEL",
+    type: "function",
+    id: "del",
+  },
+  {
+    name: "4",
+    type: "number",
+    id: "number4",
+  },
+  {
+    name: "5",
+    type: "number",
+    id: "number5",
+  },
+  {
+    name: "6",
+    type: "number",
+    id: "number6",
+  },
+  {
+    name: "+",
+    type: "operator",
+    id: "+",
+  },
+  {
+    name: "1",
+    type: "number",
+    id: "number1",
+  },
+  {
+    name: "2",
+    type: "number",
+    id: "number2",
+  },
+  {
+    name: "3",
+    type: "number",
+    id: "number3",
+  },
+  {
+    name: "-",
+    type: "operator",
+    id: "-",
+  },
+  {
+    name: ".",
+    type: "number",
+    id: ".",
+  },
+  {
+    name: "0",
+    type: "number",
+    id: "0",
+  },
+  {
+    name: "/",
+    type: "operator",
+    id: "/",
+  },
+  {
+    name: "x",
+    type: "operator",
+    id: "*",
+  },
+];
 
 function Calculator() {
   const [displayValue, setDisplayValue] = useState("0");
   const [history, setHistory] = useState(null);
   const [operator, setOperator] = useState(null);
   const [operand, setOperand] = useState(null);
+  const [isCleared, setIsCleared] = useState(true)
 
   // calculator logic goes here...
-    function handleNumberClick(number) {
-      setDisplayValue(displayValue === '0' ? number : displayValue + number);
+  function handleNumberClick(number) {
+    if(isCleared) {
+      setDisplayValue(displayValue === "0" ? number : displayValue + number);
     }
+    else{
+      setDisplayValue(number);
+      setIsCleared(true)
+    }
+  }
 
-
-    function handleOperatorClick(newOperator) {
+  function handleOperatorClick(newOperator) {
     const currentValue = parseFloat(displayValue);
     if (operand === null) {
       setOperand(currentValue);
@@ -22,6 +111,7 @@ function Calculator() {
       setDisplayValue(String(result));
     }
     setOperator(newOperator);
+    setIsCleared(false)
   }
   function operate(operator, a, b) {
     switch (operator) {
@@ -38,27 +128,31 @@ function Calculator() {
     }
   }
 
-  const numberList = [];
-  for (let i = 0; i < 10; i++) {
-    numberList.push(
-      <button key={i+'button'} onClick={() => handleNumberClick(`${i}`)}>{i}</button>
-    );
-  }
   function handleClearClick() {
     setDisplayValue("0");
     setOperator(null);
     setOperand(null);
+    setIsCleared(true)
   }
   return (
     <div>
       <div>{displayValue}</div>
-      {numberList}
-      <button onClick={() => handleClearClick()}>AC</button>
-      <button onClick={() => handleOperatorClick("+")}>+</button>
-      <button onClick={() => handleOperatorClick("-")}>*</button>
-      <button onClick={() => handleOperatorClick("/")}>/</button>
-      <button onClick={() => handleOperatorClick("*")}>*</button>
-      <button onClick={() => handleOperatorClick("=")}>=</button>
+      <div className="buttons-layout">
+      {calculatorButtons.map((item) => (
+        <button
+          key={item.id}
+          onClick={() =>
+            item.type === "number"
+              ? handleNumberClick(item.name)
+              : handleOperatorClick(item.id)
+          }
+        >
+          {item.name}
+        </button>
+      ))}
+        <button className="last-item" onClick={handleClearClick}>RESET</button>
+        <button className="last-item" onClick={handleClearClick}>=</button>
+      </div>
     </div>
   );
 }
